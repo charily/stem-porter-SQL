ea1131dc358335894fc07a945997f6a8d2092c92
charily <949987591@qq.com>
2018-09-25 13:42:10 +0800

    sss

--- mysql-porter-stemmer-master/porter-test-data.sql
+++ mysql-porter-stemmer-master/porter-test-data.sql
@@ -1,40 +1,4 @@
-/* This is the test data from the Porter Stemmer website.
-    http://tartarus.org/martin/PorterStemmer/
 
-After you run the script to create the Porter Stemmer, 
-you can run this script to load the Porter Stemmer test data
-into a table called tbltestwords.
-
-After the data is loaded, run:
-    Update tbltestwords Set Output = fnPorterAlgorithm(TestWord);
-
-Then, compare the Output against the GoldStandard:
-    Select * from tbltestwords Where GoldStandard <> Output;
-
-If it returns any rows, there was a problem.
-
-
-*/
--- MySQL dump 10.13  Distrib 5.6.12, for Win64 (x86_64)
---
--- Host: cinsql03    Database: porter
--- ------------------------------------------------------
--- Server version	5.6.21-log
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `tbltestwords`
---
 
 DROP TABLE IF EXISTS `tbltestwords`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
